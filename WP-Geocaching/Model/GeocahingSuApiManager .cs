0d2f9fe0e6beac96e989d1c4d6a3154e486157ef
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2011-11-19 07:31:26 +0000

    small refactoring

--- WP-Geocaching/Model/GeocahingSuApiManager .cs
+++ WP-Geocaching/Model/GeocahingSuApiManager .cs
@@ -22,35 +22,36 @@ namespace WP_Geocaching.Model
     /// </summary>
     public class GeocahingSuApiManager : IApiManager
     {
-        private Action<List<Cache>> setPushpinsOnMap;
+        private Action<List<Cache>> processCacheList;
         private WebClient client;
         private int id;
-        
-        public Action<List<Cache>> SetPushpinsOnMap
+
+        public Action<List<Cache>> ProcessCacheList
         {
             get
             {
-                return this.setPushpinsOnMap;
+                return this.processCacheList;
             }
             set
             {
-                this.setPushpinsOnMap = value;
+                this.processCacheList = value;
             }
         }
-      
+
         public GeocahingSuApiManager()
         {
             Random random = new Random();
             this.id = random.Next(100000000);
             this.client = new WebClient();
-            this.client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);
+            this.client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);            
         }
 
-        public void GetCacheList(double lngmax, double lgnmin, double latmax, double latmin)
+        public void GetCacheList(Action<List<Cache>> ProcessCacheList, double lngmax, double lgnmin, double latmax, double latmin)
         {
             string sUrl = "http://www.geocaching.su/pages/1031.ajax.php?exactly=1&lngmax=" + lngmax +
                 "&lngmin=" + lgnmin + "&latmax=" + latmax + "&latmin=" + latmin + "&id=" + this.id
                 + "&geocaching=f1fadbc82d0156ae0f81f7d5e0b26bda";
+            this.processCacheList = ProcessCacheList;          
             client.DownloadStringAsync(new Uri(sUrl));
         }
 
@@ -60,9 +61,9 @@ namespace WP_Geocaching.Model
             {
                 XElement caches = XElement.Parse(e.Result);
                 CacheParser parser = new CacheParser();
-                if (SetPushpinsOnMap != null)
+                if (ProcessCacheList != null)
                 {
-                    SetPushpinsOnMap(parser.Parse(caches));
+                    ProcessCacheList(parser.Parse(caches));
                 }
             }
         }
