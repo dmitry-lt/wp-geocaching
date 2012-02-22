6ddd648bf04e180596b49c10c42640b615b703a3
Nikita.Bumakov <Nikita.Bumakov@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-02-22 10:11:00 +0000

    Compass page added

---
+++ trunk/WP-Geocaching/View/Compass/CompassView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WP_Geocaching.View.Compass
+{
+    public partial class CompassView : UserControl
+    {
+        public CompassView()
+        {
+            InitializeComponent();
+        }
+    }
+}
