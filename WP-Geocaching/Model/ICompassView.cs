98b2ade6fd42c9ac85a5aa708e32b312c81a8487
Nikita.Bumakov <Nikita.Bumakov@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-02-22 16:02:31 +0000

    smooth CompassManager added

---
+++ WP-Geocaching/Model/ICompassView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WP_Geocaching.View.Compass
+{
+    public interface ICompassView
+    {
+        void SetDirection(double direction);
+    }
+}
