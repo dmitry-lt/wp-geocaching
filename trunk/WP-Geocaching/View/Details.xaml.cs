8a5f3649910fe28c8deb88f64fc849af1c24ad8f
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-02-24 21:18:29 +0000

    Added simple saving caches in DataBase (without MVVM and saving detail information)

--- trunk/WP-Geocaching/View/Details.xaml.cs
+++ trunk/WP-Geocaching/View/Details.xaml.cs
@@ -11,19 +11,40 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Windows.Navigation;
+using WP_Geocaching.ViewModel;
+using WP_Geocaching.Model;
+using WP_Geocaching.Model.DataBase;
 
 namespace WP_Geocaching
 {
     public partial class Details : PhoneApplicationPage
     {
+        DetailsViewModel detailsViewModel;
+
         public Details()
         {
             InitializeComponent();
+            this.detailsViewModel = new DetailsViewModel();
+            this.DataContext = detailsViewModel;
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            Cache cache = new Cache() { Id = Convert.ToInt32(NavigationContext.QueryString["ID"]) };
+            foreach (Cache p in  GeocahingSuApiManager.Instance.CacheList)
+            {
+                if (p.Equals(cache))
+                {
+                    detailsViewModel.Cache = p;
+                }
+            }
             this.Browser.Navigate(new Uri("http://pda.geocaching.su/cache.php?cid=" + 
                 NavigationContext.QueryString["ID"], UriKind.Absolute));
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            CacheDataBase db = new CacheDataBase();
+            db.AddNewItem(detailsViewModel.Cache);
         }
     }
 }
---
+++ trunk/WP-Geocaching/ViewModel/DetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using WP_Geocaching.Model;
+
+
+namespace WP_Geocaching.ViewModel
+{
+    public class DetailsViewModel
+    {
+        public Cache Cache;      
+        
+        public DetailsViewModel()
+        {
+        }
+    }
+}
