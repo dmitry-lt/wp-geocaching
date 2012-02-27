1dd1d5aa42547df6daa4db9518940691416409ef
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-02-27 09:08:28 +0000

    added simple Favorites Page.

---
+++ trunk/WP-Geocaching/View/Favorites.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WP_Geocaching.ViewModel;
+
+namespace WP_Geocaching.View
+{
+    public partial class Favorites : PhoneApplicationPage
+    {
+        public Favorites()
+        {
+            InitializeComponent();
+            this.DataContext = new FavoritesViewModel();
+        }
+    }
+}
---
+++ trunk/WP-Geocaching/ViewModel/FavoritesViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Collections.Generic;
+using WP_Geocaching.Model.DataBase;
+
+namespace WP_Geocaching.ViewModel
+{
+    public class FavoritesViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private CacheClass selectedCache;
+        private List<CacheClass> dataSource;
+
+        public List<CacheClass> DataSource
+        {
+            get
+            {
+                return this.dataSource;
+            }
+        }
+        public CacheClass SelectedCache
+        {
+            get { return selectedCache; }
+            set
+            {
+                bool changed = selectedCache != value;
+                if (changed)
+                {
+                    selectedCache = value;
+                    OnPropertyChanged(value.Id.ToString());
+                }
+            }
+        }
+
+        public FavoritesViewModel()
+        { 
+            CacheDataBase db = new CacheDataBase();
+            this.dataSource = db.GetCacheList();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
