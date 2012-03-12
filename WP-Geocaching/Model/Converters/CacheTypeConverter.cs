012a869821b26696d6e20bfe094209d4a8629ad6
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-03-12 19:54:18 +0000

    Favorite list modified (type and subtype in English only)

---
+++ WP-Geocaching/Model/Converters/CacheTypeConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+
+namespace WP_Geocaching.Model.Converters
+{
+    public class CacheTypeConverter : IValueConverter
+    {
+        public static string[] types = new string[]{
+            "",
+            "Традиционный",
+            "Пошаговый традиционный", 
+            "Виртуальный", 
+            "Событие", 
+            "Веб-камера", 
+            "Экстрим", 
+            "Пошаговый виртуальный", 
+            "Конкурс"
+        };
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            string st = types[System.Convert.ToInt32(value)];
+            return st;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
+
---
+++ WP-Geocaching/ViewModel/FavoriteClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using WP_Geocaching.Model.DataBase;
+
+namespace WP_Geocaching.ViewModel
+{
+    public class FavoriteClass : DbCacheItem
+    {
+        public Uri IconUri { get; set; }
+        public FavoriteClass(DbCacheItem item)
+        {
+            this.Id = item.Id;
+            this.Latitude = item.Latitude;
+            this.Longitude = item.Longitude;
+            this.Name = item.Name;
+            this.Subtype = item.Subtype;
+            this.Type = item.Type;
+            this.Details = item.Details;
+            this.IconUri = new Uri((item.Type.ToString() + item.Subtype.ToString()), UriKind.Relative);
+        }
+    }
+}
