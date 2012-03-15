31b171f57b9551b2486981392edae308ddf838eb
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-03-15 15:29:17 +0000

    Resources refactoring

--- WP-Geocaching/Model/Converters/CacheSubtypeConverter.cs
+++ WP-Geocaching/Model/Converters/CacheSubtypeConverter.cs
@@ -9,16 +9,17 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Data;
+using WP_Geocaching.Resources.Localization;
 
 namespace WP_Geocaching.Model.Converters
 {
     public class CacheSubtypeConverter : IValueConverter
     {
         public static string[] subtypes = new string[]{
-            "", 
-            "Действующий", 
-            "Сомнительный", 
-            "Недействующий"
+            null, 
+            CacheSubtypeResources.valid, 
+            CacheSubtypeResources.not_confirmed, 
+            CacheSubtypeResources.not_valid
         };
 
         public object Convert(object value, Type targetType, object parameter,
