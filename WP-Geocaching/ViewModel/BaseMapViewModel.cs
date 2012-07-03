1f4925859386c6ea9a7d83a6b6d9408bf15a608a
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-07-03 12:01:41 +0000

    BaseMapViewModel added, some class members are inherited

---
+++ WP-Geocaching/ViewModel/BaseMapViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+using System.Device.Location;
+
+namespace WP_Geocaching.ViewModel
+{
+    public class BaseMapViewModel : BaseViewModel
+    {
+        protected GeoCoordinate mapCenter;
+        protected GeoCoordinate currentLocation;
+        protected Visibility undetectedLocationMessageVisibility = Visibility.Collapsed;
+
+        public Visibility UndetectedLocationMessageVisibility
+        {
+            get
+            {
+                return this.undetectedLocationMessageVisibility;
+            }
+            set
+            {
+                undetectedLocationMessageVisibility = value;
+                NotifyPropertyChanged("UndetectedLocationMessageVisibility");
+            }
+        }
+
+        public GeoCoordinate MapCenter
+        {
+            get
+            {
+                return this.mapCenter;
+            }
+            set
+            {
+                mapCenter = value;
+                NotifyPropertyChanged("MapCenter");
+            }
+        }
+
+        public void SetMapCenterOnCurrentLocationOrShowMessage(Dispatcher dispatcher)
+        {
+            if (currentLocation == null)
+            {
+                UndetectedLocationMessageVisibility = Visibility.Visible;
+                var timer = new Timer((state) =>
+                {
+                    var t = (Timer)state;
+                    dispatcher.BeginInvoke(() =>
+                    {
+                        UndetectedLocationMessageVisibility = Visibility.Collapsed;
+                    });
+                    t.Dispose();
+                });
+                timer.Change(3000, 0);
+            }
+            else
+            {
+                MapCenter = currentLocation;
+            }
+        }
+    }
+}
