ce8472984eab59ccf421a392134a1e1074c942cb
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-07-11 06:20:42 +0000

    other photo display way added (some binding troubles)

---
+++ WP-Geocaching/Model/PreviewImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WP_Geocaching.Model
+{
+    public class PreviewImage
+    {
+        public ImageSource Source { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
---
+++ WP-Geocaching/Model/ThreePhotos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WP_Geocaching.Model
+{
+    public class ThreePhotos
+    {
+        public int Count { get; private set; }
+        public PreviewImage FirstPhoto { get; private set; }
+        public PreviewImage SecondPhoto { get; private set; }
+        public PreviewImage ThirdPhoto { get; private set; }
+
+        public ThreePhotos()
+        {
+            Count = 0;
+        }
+
+        public void Add(PreviewImage image)
+        {
+            switch (Count)
+            {
+                case 0:
+                    {
+                        FirstPhoto = image;
+                        Count++;
+                        return;
+                    };
+                case 1:
+                    {
+                        SecondPhoto = image;
+                        Count++;
+                        return;
+                    }
+                case 2:
+                    {
+                        ThirdPhoto = image;
+                        Count++;
+                        return;
+                    }
+            }
+        }
+
+        public bool isFull()
+        {
+            return (Count == 3);
+        }
+    }
+}
