bcc2c62b77b924fc4242114c55de28a752f84ea9
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-07-18 11:32:51 +0000

    downloading photos from IS bug fixed

--- WP-Geocaching/Model/GeocahingSuApiManager.cs
+++ WP-Geocaching/Model/GeocahingSuApiManager.cs
@@ -195,7 +195,7 @@ namespace WP_Geocaching.Model
 
             if (processAction != null)
             {
-                processAction(photoUrls);
+                processAction(photoNames);
             }
         }
 
