3984b127689a70f3b4cb30cb831b10614215da16
Ilya.Noskov.91@gmail.com <Ilya.Noskov.91@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-07-19 11:02:52 +0000

    Sexagesimal and Decimal converters added. CoordinateInput updated.

---
+++ CoordinateInput/CoordinateInput/Sexagesimal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+public class Sexagesimal
+{
+    private int degrees;
+    private int minutes;
+    private double minutesFraction;
+
+    public int Degrees
+    {
+        get
+        {
+            return degrees;
+        }
+        set
+        {
+            degrees = value;
+        }
+    }
+
+    public int Minutes
+    {
+        get
+        {
+            return minutes;
+        }
+        set
+        {
+            minutes = value;
+        }
+    }
+
+    public double MinutesFraction
+    {
+        get
+        {
+            return minutesFraction;
+        }
+        set
+        {
+            minutesFraction = value;
+        }
+    }
+
+    public Sexagesimal(double coordinate)
+    {
+        degrees = (int)coordinate;
+        minutes = (int)((coordinate - degrees) * 60);
+        minutesFraction = Math.Round((coordinate - degrees) * 60 - minutes, 3);
+    }
+
+    public double ToCoordinate()
+    {
+        return degrees + (minutes + minutesFraction) / 60.0;
+    }
+}
