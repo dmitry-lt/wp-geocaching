8c4cd8fea479971b13a988712e0a0b4eb031c1ec
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-07-25 08:49:44 +0000

    Issue 34: email sending added

---
+++ trunk/WP-Geocaching/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Input;
+using Microsoft.Phone.Tasks;
+using WP_Geocaching.Model;
+using WP_Geocaching.Resources.Localization;
+
+namespace WP_Geocaching.ViewModel
+{
+    public class SettingsViewModel : BaseMapViewModel
+    {
+        public ICommand SendEmailCommand { get; private set; }
+
+        public SettingsViewModel()
+        {
+            SendEmailCommand = new ButtonCommand(SendEmail);
+            
+        }
+
+        private void SendEmail(object p)
+        {
+            var emailComposeTask = new EmailComposeTask
+            {
+                Subject = "[Geocaching]",
+                To = AppResources.SupportEmail
+            };
+
+            emailComposeTask.Show();
+        }
+    }
+
+
+}
