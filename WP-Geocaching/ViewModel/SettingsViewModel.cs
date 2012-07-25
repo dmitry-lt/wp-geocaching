7983ecced26cb51460794a911133e7a14f0072ac
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-07-25 09:15:18 +0000

    Issue 34: map mode settings saving added

--- WP-Geocaching/ViewModel/SettingsViewModel.cs
+++ WP-Geocaching/ViewModel/SettingsViewModel.cs
@@ -7,23 +7,51 @@ namespace WP_Geocaching.ViewModel
 {
     public class SettingsViewModel : BaseMapViewModel
     {
+        private Settings settings;
+
         public ICommand SendEmailCommand { get; private set; }
+        public ICommand ChooseRoadModeCommand { get; private set; }
+        public ICommand ChooseAerialModeCommand { get; private set; }
+        public bool IsAerialChecked { get; private set; }
+        public bool IsRoadChecked { get; private set; }
 
         public SettingsViewModel()
         {
+            settings = new Settings();
+
             SendEmailCommand = new ButtonCommand(SendEmail);
-            
+            ChooseRoadModeCommand = new ButtonCommand(ChooseRoadMode);
+            ChooseAerialModeCommand = new ButtonCommand(ChooseAerialMode);
+
+            if (settings.MapMode == MapMode.Road)
+            {
+                IsRoadChecked = true;
+            }
+            else
+            {
+                IsAerialChecked = true;
+            }
         }
 
         private void SendEmail(object p)
         {
             var emailComposeTask = new EmailComposeTask
             {
-                Subject = "[Geocaching]",
+                Subject = AppResources.EmailSubject,
                 To = AppResources.SupportEmail
             };
 
             emailComposeTask.Show();
+        }
+
+        private void ChooseRoadMode(object p)
+        {
+            settings.MapMode = MapMode.Road;
+        }
+
+        private void ChooseAerialMode(object p)
+        {
+            settings.MapMode = MapMode.Aerial;
         }
     }
 
