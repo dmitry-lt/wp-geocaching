f3ada7f030f877dc94cc7fb117be3fd6f86112e2
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-07-31 14:12:34 +0000

    issue 41 fixed, MainPage refactoring

--- WP-Geocaching/MainPage.xaml.cs
+++ WP-Geocaching/MainPage.xaml.cs
@@ -1,76 +1,25 @@
-﻿using System;
-using System.Threading;
-using System.Windows;
+﻿using System.Windows;
 using Microsoft.Phone.Controls;
-using WP_Geocaching.Model;
-using WP_Geocaching.Model.DataBase;
+using WP_Geocaching.ViewModel.MainPageViewModel;
 
 namespace WP_Geocaching
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private MainPageViewModel viewModel;
+
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        private void ChooseCacheClick(object sender, RoutedEventArgs e)
-        {
-            NavigationManager.Instance.NavigateToBingMap();
-        }
-
-        private void SettingsClick(object sender, RoutedEventArgs e)
-        {
-            NavigationManager.Instance.NavigateToSettings();
-        }
-
-        private void FavoritesClick(object sender, RoutedEventArgs e)
-        {
-            var db = new CacheDataBase();
-            var dbCacheList = db.GetCacheList();
-
-            if (dbCacheList.Count > 0)
-            {
-                NavigationManager.Instance.NavigateToFavorites();
-            }
-            else
-            {
-                NoFavoriteCachesMessage.Visibility = Visibility.Visible;
-                var timer = new Timer(DisposeTimerAndCollapseMessage);
-                timer.Change(3000, 0);
-            }
-        }
-
-        private void SearchCacheClick(object sender, RoutedEventArgs e)
-        {
-            var settings = new Settings();
-            if (settings.LastSoughtCacheId > 0)
-            {
-                NavigationManager.Instance.NavigateToSearchBingMap(settings.LastSoughtCacheId.ToString());
-            }
-            else
-            {
-                NoSouhgtCacheMessage.Visibility = Visibility.Visible;
-                var timer = new Timer(DisposeTimerAndCollapseMessage);
-                timer.Change(3000, 0);
-            }
-        }
-
-        private void DisposeTimerAndCollapseMessage(object state)
-        {
-            var t = (Timer)state;
-            t.Dispose();
-            Dispatcher.BeginInvoke(() =>
-            {
-                NoSouhgtCacheMessage.Visibility = Visibility.Collapsed;
-                NoFavoriteCachesMessage.Visibility = Visibility.Collapsed;
-            });
+            viewModel = new MainPageViewModel();
+            DataContext = viewModel;
         }
 
         protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
         {
-            NoSouhgtCacheMessage.Visibility = Visibility.Collapsed;
-            NoFavoriteCachesMessage.Visibility = Visibility.Collapsed;
+            viewModel.NoSouhgtCacheMessageVisibility = Visibility.Collapsed;
+            viewModel.NoFavoriteCachesMessageVisibility = Visibility.Collapsed;
         }
     }
 }
--- WP-Geocaching/ViewModel/FavoritesViewModel.cs
+++ WP-Geocaching/ViewModel/FavoritesViewModel.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.ComponentModel;
-using System.Data.Linq;
-using System.Data.Linq.Mapping;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using WP_Geocaching.Model.DataBase;
 using WP_Geocaching.Model;
 
@@ -26,7 +13,7 @@ namespace WP_Geocaching.ViewModel
         {
             get
             {
-                return this.dataSource;
+                return dataSource;
             }
             private set
             {
