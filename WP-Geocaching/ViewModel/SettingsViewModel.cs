7d48bfe4223db6a0d8591bc6120ca66857981816
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-10-20 13:59:19 +0000

    Merged revision(s)  from branches/dev_opencaching_com

--- WP-Geocaching/ViewModel/SettingsViewModel.cs
+++ WP-Geocaching/ViewModel/SettingsViewModel.cs
@@ -32,6 +32,50 @@ namespace WP_Geocaching.ViewModel
             }
         }
 
+        private bool CanAssignCacheProviderValue(bool value)
+        {
+            if (value)
+            {
+                return true;
+            }
+
+            var count = 0;
+            if (IsOpenCachingComEnabled)
+            {
+                count++;
+            }
+            if (IsGeocachingSuEnabled)
+            {
+                count++;
+            }
+            return count > 1;
+        }
+
+        public bool IsOpenCachingComEnabled
+        {
+            get { return settings.IsOpenCachingComEnabled; }
+            set
+            {
+                if (CanAssignCacheProviderValue(value))
+                {
+                    settings.IsOpenCachingComEnabled = value;
+                }
+                NotifyPropertyChanged("IsOpenCachingComEnabled");
+            }
+        }
+        public bool IsGeocachingSuEnabled 
+        {
+            get { return settings.IsGeocachingSuEnabled; }
+            set
+            {
+                if (CanAssignCacheProviderValue(value))
+                {
+                    settings.IsGeocachingSuEnabled = value;
+                }
+                NotifyPropertyChanged("IsGeocachingSuEnabled");
+            }
+        }
+
         public SettingsViewModel()
         {
             settings = new Settings();
