43e2d84f9b60a078d16adf687e121b5b7b30a9f6
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-10-20 14:43:52 +0000

    feature branch removed

--- branches/dev_opencaching_com/WP-Geocaching/Model/Api/CacheProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WP_Geocaching.Model.Api
-{
-    public enum CacheProvider
-    {
-        GeocachingSu, OpenCachingCom
-    }
-}
--- branches/dev_opencaching_com/WP-Geocaching/Model/ICompassAware.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WP_Geocaching.View.Compass
-{
-    public interface ICompassAware
-    {
-        void SetDirection(double direction);
-    }
-}
