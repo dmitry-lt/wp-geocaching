e81d748f55af7d46480a8c6c44fe8fd1b092bd9d
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-01 14:58:52 +0000

    google analytics

---
+++ branches/dev_google_analytics/GeocachingPlus/Model/Analytics/AnalyticsService.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Composition.Hosting;
+using System.Windows;
+using Google.WebAnalytics;
+
+namespace GeocachingPlus.Model.Analytics
+{
+    public class AnalyticsService : IApplicationService
+    {
+        #region IApplicationService Members
+
+        public void StartService(ApplicationServiceContext context)
+        {
+            CompositionHost.Initialize(
+                new AssemblyCatalog(Application.Current.GetType().Assembly),
+                new AssemblyCatalog(typeof(Microsoft.WebAnalytics.AnalyticsEvent).Assembly),
+                new AssemblyCatalog(typeof(Microsoft.WebAnalytics.Behaviors.TrackAction).Assembly),
+                new AssemblyCatalog(typeof(GoogleAnalytics).Assembly));
+        }
+
+        public void StopService()
+        {
+        }
+
+        #endregion
+    }
+}
