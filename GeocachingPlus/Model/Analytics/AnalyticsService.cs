599fcf6cd77c1545f0f770c1f1e8d9f7d221c4c6
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-01 15:44:48 +0000

    google analytics

--- GeocachingPlus/Model/Analytics/AnalyticsService.cs
+++ GeocachingPlus/Model/Analytics/AnalyticsService.cs
@@ -12,6 +12,21 @@ namespace GeocachingPlus.Model.Analytics
         private readonly IApplicationService _innerService;
         private readonly GoogleAnalytics _googleAnalytics;
 
+        private const string DebugTrackingId = "UA-36031366-2";
+        private const string ReleaseTrackingId = "UA-36031366-1";
+
+        public string GoogleAnalyticsTrackingId
+        {
+
+            get
+            {
+#if DEBUG
+                return DebugTrackingId;
+#endif
+                return ReleaseTrackingId;
+            }
+        }
+
         public AnalyticsService()
         {
             _googleAnalytics = new GoogleAnalytics();
@@ -19,17 +34,14 @@ namespace GeocachingPlus.Model.Analytics
             _googleAnalytics.CustomVariables.Add(new PropertyValue { PropertyName = "Application Version", Value = AnalyticsProperties.ApplicationVersion });
             _googleAnalytics.CustomVariables.Add(new PropertyValue { PropertyName = "Device OS", Value = AnalyticsProperties.OsVersion });
             _googleAnalytics.CustomVariables.Add(new PropertyValue { PropertyName = "Device", Value = AnalyticsProperties.Device });
+
+            _googleAnalytics.WebPropertyId = GoogleAnalyticsTrackingId;
+
             _innerService = new WebAnalyticsService
             {
                 IsPageTrackingEnabled = false,
                 Services = { _googleAnalytics, }
             };
-        }
-
-        public string WebPropertyId
-        {
-            get { return _googleAnalytics.WebPropertyId; }
-            set { _googleAnalytics.WebPropertyId = value; }
         }
 
         #region IApplicationService Members
