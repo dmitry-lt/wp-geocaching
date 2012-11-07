ceb476d2daf1f91cd02572379c5db14aec06cc6b
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-07 20:57:40 +0000

    branch 1.0.2

---
+++ WP-Geocaching/Model/ICompassAware.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WP_Geocaching.View.Compass
+{
+    public interface ICompassAware
+    {
+        void SetDirection(double direction);
+    }
+}
---
+++ WP-Geocaching/ViewModel/PhotoGalleryPageViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using WP_Geocaching.Model;
+
+namespace WP_Geocaching.ViewModel
+{
+    public class PhotoGalleryPageViewModel : BaseViewModel
+    {
+        private double maxHeight;
+        private ImageSource imageSource;
+        private int currentIndex;
+
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return imageSource;
+            }
+            set
+            {
+                imageSource = value;
+                NotifyPropertyChanged("ImageSource");
+            }
+        }
+
+        public double MaxHeight
+        {
+            get
+            {
+                return maxHeight;
+            }
+            set
+            {
+                maxHeight = value;
+                NotifyPropertyChanged("MaxHeight");
+            }
+        }
+
+        public void LoadFullsizePhoto(int index)
+        {
+            currentIndex = index;
+            GeocahingSuApiManager.Instance.ProcessPhoto(SetImageSource, index);
+        }
+
+        public void SetImageSource(Photo source)
+        {
+            ImageSource = source.PhotoSource;
+            MaxHeight = ((BitmapSource)ImageSource).PixelHeight * Math.Sqrt(2);
+        }
+
+        public void LoadNext(Action action)
+        {
+            action();
+            LoadFullsizePhoto(++currentIndex);
+        }
+
+        public void LoadPrevious(Action action)
+        {
+            action();
+            LoadFullsizePhoto(--currentIndex);
+        }
+    }
+}
