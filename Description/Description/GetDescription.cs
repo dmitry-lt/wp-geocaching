d8ca4f0462c9c3d354f4cdf8bd8c62006dafab20
Yekaterina.Kanaeva@gmail.com <Yekaterina.Kanaeva@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-08 20:00:10 +0000

    add empty project

---
+++ Description/Description/GetDescription.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Description
+{
+    class GetDescription
+    {
+        private const string InfoUrl = http://www.geocaching.com/seek/cache_details.aspx?wp=0;
+    }
+}
