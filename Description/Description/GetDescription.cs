12dc56d008cf9fa7bc8f1e79000ed4be708ed2bf
Yekaterina.Kanaeva@gmail.com <Yekaterina.Kanaeva@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-11 19:35:52 +0000

    get html-page, but it isn't printed on the screen.

--- Description/Description/GetDescription.cs
+++ Description/Description/GetDescription.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Globalization;
 
 namespace Description
 {
     class GetDescription
     {
-        private const string InfoUrl = http://www.geocaching.com/seek/cache_details.aspx?wp=0;
+         public void FetchCacheDetails(String cacheId)
+        {
+            string sUrl = InfoUrl + cacheId;
+            var client = new WebClient();
+
+            client.DownloadStringCompleted += (sender, e) =>
+            {
+                if (e.Error != null) return;
+                MainPage page = new MainPage();
+                page.textBox1.Text = e.Result;
+            };
+            client.DownloadStringAsync(new Uri(sUrl));
+        }
+
+        private const string InfoUrl = "http://www.geocaching.com/seek/cache_details.aspx?wp=";
     }
 }
