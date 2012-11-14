6eeaaa7e9967e0c28e305055467e711e067c95f0
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-14 10:53:08 +0000

    geocaching.com

---
+++ branches/dev_geocaching_com/GeocachingPlus/Model/Api/GeocachingCom/ColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace GeocachingPlus.Model.Api.GeocachingCom
+{
+    public static class ColorExtensions
+    {
+        public static int ToArgb(this Color color)
+        {
+            return (color.A << 24) | (color.R << 16) | (color.G << 8) | color.B;
+        }
+
+    }
+}
