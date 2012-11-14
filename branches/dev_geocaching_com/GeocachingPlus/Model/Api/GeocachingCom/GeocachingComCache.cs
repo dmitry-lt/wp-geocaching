bc205d5878cc0e5576aba48367413e370e64aa04
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-14 11:03:08 +0000

    geocaching.com

--- branches/dev_geocaching_com/GeocachingPlus/Model/Api/GeocachingCom/GeocachingComCache.cs
+++ branches/dev_geocaching_com/GeocachingPlus/Model/Api/GeocachingCom/GeocachingComCache.cs
@@ -2,6 +2,28 @@
 {
     public class GeocachingComCache : Cache
     {
+        public enum Types
+        {
+            TRADITIONAL = 201,
+            MULTI = 202,
+            MYSTERY = 203,
+            LETTERBOX = 204,
+            EVENT = 205,
+            MEGA_EVENT = 206,
+            EARTH = 207,
+            CITO = 208,
+            WEBCAM = 209,
+            VIRTUAL = 210,
+            WHERIGO = 211,
+            LOSTANDFOUND = 212,
+            PROJECT_APE = 213,
+            GCHQ = 214,
+            GPS_EXHIBIT = 215,
+            UNKNOWN = 216,
+        }
+
+        public Types Type { get; set; }
+
         public bool ReliableLocation { get; set; }
 
         public GeocachingComCache()
