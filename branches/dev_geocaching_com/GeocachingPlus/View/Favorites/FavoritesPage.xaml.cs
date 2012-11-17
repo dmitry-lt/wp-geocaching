b1d65370dd34c4e6b85cff2a3126462f191f8aa0
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-17 21:21:25 +0000

    geocaching.com

--- branches/dev_geocaching_com/GeocachingPlus/View/Favorites/FavoritesPage.xaml.cs
+++ branches/dev_geocaching_com/GeocachingPlus/View/Favorites/FavoritesPage.xaml.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
+﻿using Microsoft.Phone.Controls;
 using GeocachingPlus.ViewModel;
 using System.Windows.Navigation;
 
@@ -28,7 +17,7 @@ namespace GeocachingPlus.View.Favorites
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            if (e.NavigationMode == System.Windows.Navigation.NavigationMode.Back)
+            if (e.NavigationMode == NavigationMode.Back)
             {
                 favoritesViewModel.UpdateDataSource();
             }
