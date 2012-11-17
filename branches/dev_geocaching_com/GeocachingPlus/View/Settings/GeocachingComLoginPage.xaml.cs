167d9220957ea481ff2a3955ae1cfcdeefeb3491
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-17 22:30:32 +0000

    geocaching.com

--- branches/dev_geocaching_com/GeocachingPlus/View/Settings/GeocachingComLoginPage.xaml.cs
+++ branches/dev_geocaching_com/GeocachingPlus/View/Settings/GeocachingComLoginPage.xaml.cs
@@ -1,26 +1,14 @@
 ﻿using Microsoft.Phone.Controls;
 using GeocachingPlus.ViewModel;
-using System.Windows.Navigation;
 
 namespace GeocachingPlus.View.Settings
 {
     public partial class GeocachingComLoginPage : PhoneApplicationPage
     {
-        FavoritesViewModel favoritesViewModel;
-
         public GeocachingComLoginPage()
         {
             InitializeComponent();
-            favoritesViewModel = new FavoritesViewModel();
-            this.DataContext = favoritesViewModel;
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            if (e.NavigationMode == NavigationMode.Back)
-            {
-                favoritesViewModel.UpdateDataSource();
-            }
+            DataContext = new GeocachingComLoginPageViewModel();
         }
     }
 }
---
+++ branches/dev_geocaching_com/GeocachingPlus/ViewModel/GeocachingComLoginPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace GeocachingPlus.ViewModel
+{
+    public class GeocachingComLoginPageViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public ICommand LoginCommand { get; set; }
+    }
+}
