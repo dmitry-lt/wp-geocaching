98f799db41ee5bb741c166c8c16bf7f473fbae68
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-18 08:48:23 +0000

    geocaching.com

---
+++ GeocachingPlus/Model/Api/GeocachingCom/UrlHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace GeocachingPlus.Model.Api.GeocachingCom
+{
+    public class UrlHelper
+    {
+        public static string FormUrl(string baseUrl, Dictionary<string, string> parameters)
+        {
+            var urlString = baseUrl;
+            var firstParameter = true;
+            foreach (var k in parameters.Keys)
+            {
+                if (firstParameter)
+                {
+                    urlString += "?" + k + "=" + parameters[k];
+                    firstParameter = false;
+                }
+                else
+                {
+                    urlString += "&" + k + "=" + parameters[k];
+                }
+            }
+            return urlString;
+        }
+    }
+}
