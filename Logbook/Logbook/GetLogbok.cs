eb7cbab783cbe71133508e88c1ee32569e4f06d0
Yekaterina.Kanaeva@gmail.com <Yekaterina.Kanaeva@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-19 21:10:44 +0000

    Nothing happens

---
+++ Logbook/Logbook/GetLogbok.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Logbook
+{
+    public class GetLogbook
+    {
+        public void FetchCacheDetails(Action<String> processLogbook, String cacheId)
+        {
+            string sUrl = InfoUrl + cacheId;
+            var client = new WebClient();
+
+            client.DownloadStringCompleted += (sender, e) =>
+            {
+                if (e.Error != null) return;
+                String patternLogbook = "initalLogs = (\\{.+\\});";
+                if (null != processLogbook)
+                {
+                   var logbook = Regex.Matches(e.Result, patternLogbook, RegexOptions.Singleline)[0].Value;
+                    processLogbook(logbook);
+                }
+            };
+            client.DownloadStringAsync(new Uri(sUrl));
+        }
+
+        private const string InfoUrl = "http://www.geocaching.com/seek/cache_details.aspx?wp=";
+    }
+}
---
+++ Logbook/Logbook/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Logbook
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void showText(String text)
+        {
+            webBrowser1.NavigateToString(text);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            GetLogbook test = new GetLogbook();
+            Action<string> processLogbook;
+            processLogbook = delegate(string s) { showText(s); };
+            test.FetchCacheDetails(processLogbook, "GC1NKDN");
+        }
+    }
+}
