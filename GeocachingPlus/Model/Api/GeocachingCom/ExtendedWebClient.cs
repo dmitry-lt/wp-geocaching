ad7943dd3858b6b3da6fb813eb07a30cba3a7245
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-21 16:05:32 +0000

    geocaching.com

--- GeocachingPlus/Model/Api/GeocachingCom/ExtendedWebClient.cs
+++ GeocachingPlus/Model/Api/GeocachingCom/ExtendedWebClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.Browser;
 using System.Security;
 using System.Text;
 using System.Windows.Navigation;
@@ -130,118 +131,54 @@ namespace GeocachingPlus.Model.Api.GeocachingCom
             }
         }
 
-
-/*
-        public void Post(string address, string parameters, Action<string> onResponseGot)
-        {
-            var uri = new Uri(address);
-
-
-            var getResponseCallback = 
-                req =>
-                    {
-                        
-                    }
-
-            var request = (HttpWebRequest)WebRequest.Create(uri); ;
-            request.Method = "POST";
-
-            // End the operation
-            Stream postStream = request.EndGetRequestStream(asynchronousResult);
-
-            // Convert the string into a byte array.
-            byte[] postBytes = Encoding.UTF8.GetBytes(post);
-
-            // Write to the request stream.
-            postStream.Write(postBytes, 0, postBytes.Length);
-            postStream.Close();
-
-            // Start the asynchronous operation to get the response
-            request.BeginGetResponse(new AsyncCallback(getResponseCallback), request);
-
-            r.BeginGetRequestStream(delegate(IAsyncResult req)
-            {
-                var outStream = r.EndGetRequestStream(req);
-
-                var bytes = GetBytes(parameters);
-                outStream.Write(bytes, 0, bytes.Length);
-                outStream.Flush();
-
-                r.BeginGetResponse(delegate(IAsyncResult result)
-                {
-                    try
-                    {
-                        var response = (HttpWebResponse) r.EndGetResponse(result);
-
-                        using (var stream = response.GetResponseStream())
-                        {
-                            using (var reader = new StreamReader(stream))
-                            {
-                                onResponseGot(reader.ReadToEnd());
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        onResponseGot(null);
-                    }
-
-                }, null);
-
-            }, null);
-        }
-*/
-
         public void Post(string address, Dictionary<string, string> parameters, Action<string> onResponseGot)
         {
             var stringParams = UrlHelper.FormUrlParameterQuery(parameters);
             Post(address, stringParams, onResponseGot);
         }
-    }
 
+        public void Get(string url, Action<string> onResponseGot)
+        {
+            var cookies = new CookieContainer();
 
-    public class AsciiEncoding : System.Text.Encoding
-    {
-        public override int GetMaxByteCount(int charCount)
-        {
-            return charCount;
-        }
-        public override int GetMaxCharCount(int byteCount)
-        {
-            return byteCount;
-        }
-        public override int GetByteCount(char[] chars, int index, int count)
-        {
-            return count;
-        }
-        public override byte[] GetBytes(char[] chars)
-        {
-            return base.GetBytes(chars);
-        }
-        public override int GetCharCount(byte[] bytes)
-        {
-            return bytes.Length;
-        }
-        public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
-        {
-            for (int i = 0; i < charCount; i++)
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.CookieContainer = cookies;
+
+            request.BeginGetResponse(new AsyncCallback((ar) =>
             {
-                bytes[byteIndex + i] = (byte)chars[charIndex + i];
-            }
-            return charCount;
+                var httpWebRequest = (HttpWebRequest)ar.AsyncState;
+                if (httpWebRequest != null)
+                {
+                    using (var response = (HttpWebResponse)httpWebRequest.EndGetResponse(ar))
+                    {
+                        Stream streamResponse;
+                        if ("gzip".Equals(response.Headers["Content-Encoding"]))
+                        {
+                            streamResponse = new GZipInputStream(response.GetResponseStream());
+                        }
+                        else
+                        {
+                            streamResponse = response.GetResponseStream();
+                        }
+                        using (streamResponse)
+                        {
+                            using (var streamRead = new StreamReader(streamResponse))
+                            {
+                                var responseString = streamRead.ReadToEnd();
+                                onResponseGot(responseString);
+                            }
+                        }
+                    }
+                }
+            }), request);
         }
-        public override int GetCharCount(byte[] bytes, int index, int count)
-        {
-            return count;
-        }
-        public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
-        {
-            for (int i = 0; i < byteCount; i++)
-            {
-                chars[charIndex + i] = (char)bytes[byteIndex + i];
-            }
-            return byteCount;
-        }
-    }
 
+        public void Get(string address, Dictionary<string, string> parameters, Action<string> onResponseGot)
+        {
+            var url = UrlHelper.FormUrl(address, parameters);
+            Get(url, onResponseGot);
+        }
+
+    }
 }
