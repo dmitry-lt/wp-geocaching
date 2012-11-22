bbbd0278806abf977e6540ffba330b5ea36ddaf7
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-22 22:10:49 +0000

    geocaching.com

--- GeocachingPlus/View/Settings/GeocachingComLoginPage.xaml.cs
+++ GeocachingPlus/View/Settings/GeocachingComLoginPage.xaml.cs
@@ -9,14 +9,31 @@ namespace GeocachingPlus.View.Settings
         public GeocachingComLoginPage()
         {
             InitializeComponent();
+            Subscribe();
+        }
+
+        private void Subscribe()
+        {
+            var vm = ((GeocachingComLoginPageViewModel)DataContext);
+            vm.LoginSucceeded += GoBack;
+        }
+
+        private void Unsubscribe()
+        {
             var vm = ((GeocachingComLoginPageViewModel)DataContext);
             vm.LoginSucceeded -= GoBack;
-            vm.LoginSucceeded += GoBack;
         }
 
         private void GoBack(object sender, EventArgs e)
         {
+            Unsubscribe();
             NavigationService.GoBack();
         }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            Unsubscribe();
+        }
+
     }
 }
