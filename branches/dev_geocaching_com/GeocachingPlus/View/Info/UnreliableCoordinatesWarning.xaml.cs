fe157c8e59222c85413b380e401ada9cbd1b1285
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-11-23 10:56:52 +0000

    geocaching.com

---
+++ branches/dev_geocaching_com/GeocachingPlus/View/Info/UnreliableCoordinatesWarning.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GeocachingPlus.View.Info
+{
+    public partial class UnreliableCoordinatesWarning : UserControl
+    {
+        public UnreliableCoordinatesWarning()
+        {
+            InitializeComponent();
+        }
+    }
+}
