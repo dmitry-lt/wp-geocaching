0de0db23d9ce58d2b8c28cf0a8ce68c62818bccd
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-12-03 19:01:50 +0000

    geocaching.com

---
+++ GeocachingPlus/ViewModel/ServicesViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Input;
+using GeocachingPlus.Model;
+using GeocachingPlus.Model.Navigation;
+
+namespace GeocachingPlus.ViewModel
+{
+    public class ServicesViewModel : BaseViewModel
+    {
+        private Settings settings = new Settings();
+
+        private void ShowGeocachingComLogin(object p)
+        {
+            NavigationManager.Instance.NavigateToGeocachingComLogin();
+        }
+        
+        public ICommand ShowGeocachingComLoginCommand { get { return new ButtonCommand(ShowGeocachingComLogin); } }
+
+        private bool CanAssignCacheProviderValue(bool value)
+        {
+            if (value)
+            {
+                return true;
+            }
+
+            var count = 0;
+            if (IsOpenCachingComEnabled)
+            {
+                count++;
+            }
+            if (IsGeocachingSuEnabled)
+            {
+                count++;
+            }
+            if (IsGeocachingComEnabled)
+            {
+                count++;
+            }
+            return count > 1;
+        }
+
+        public bool IsOpenCachingComEnabled
+        {
+            get { return settings.IsOpenCachingComEnabled; }
+            set
+            {
+                if (CanAssignCacheProviderValue(value))
+                {
+                    settings.IsOpenCachingComEnabled = value;
+                }
+                RaisePropertyChanged(() => IsOpenCachingComEnabled);
+            }
+        }
+        public bool IsGeocachingSuEnabled
+        {
+            get { return settings.IsGeocachingSuEnabled; }
+            set
+            {
+                if (CanAssignCacheProviderValue(value))
+                {
+                    settings.IsGeocachingSuEnabled = value;
+                }
+                RaisePropertyChanged(() => IsGeocachingSuEnabled);
+            }
+        }
+
+        public bool IsGeocachingComEnabled
+        {
+            get { return settings.IsGeocachingComEnabled; }
+            set
+            {
+                if (CanAssignCacheProviderValue(value))
+                {
+                    settings.IsGeocachingComEnabled = value;
+                }
+                RaisePropertyChanged(() => IsGeocachingComEnabled);
+            }
+        }
+
+    }
+}
