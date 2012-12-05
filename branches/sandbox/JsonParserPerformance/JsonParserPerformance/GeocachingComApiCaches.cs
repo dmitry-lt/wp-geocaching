d9cd90dd45c1e1d67c0ac5eb488f614941c2923c
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-12-05 21:29:47 +0000

    JSON parser

---
+++ branches/sandbox/JsonParserPerformance/JsonParserPerformance/GeocachingComApiCaches.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace JsonParserPerformance
+{
+    [DataContract]
+    public class GeocachingComApiCaches
+    {
+        [DataMember]
+        public string[] grid { get; set; }
+
+        [DataMember]
+        public string[] keys { get; set; }
+
+        [DataMember]
+        public Dictionary<string, GeocachingComApiCache[]> data { get; set; }
+    }
+
+    [DataContract]
+    public class GeocachingComApiCache
+    {
+        [DataMember]
+        public string i { get; set; }
+
+        [DataMember]
+        public string n { get; set; }
+    }
+}
---
+++ branches/sandbox/JsonParserPerformance/JsonParserPerformance/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using Newtonsoft.Json;
+
+namespace JsonParserPerformance
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private double DiffMillis(DateTime from, DateTime to)
+        {
+            var ts = new TimeSpan(to.Ticks - from.Ticks);
+            return ts.TotalMilliseconds;
+        }
+
+        private const int Count = 100;
+
+        private double Measure(Action action)
+        {
+            var from = DateTime.Now;
+            for (var i = 0; i <= Count; i++)
+            {
+                var parsedData = (GeocachingComApiCaches)JsonConvert.DeserializeObject(Data.Json, typeof(GeocachingComApiCaches));
+            }
+            return DiffMillis(from, DateTime.Now);
+        }
+
+        private void NewtonsoftJson()
+        {
+            var parsedData = (GeocachingComApiCaches)JsonConvert.DeserializeObject(Data.Json, typeof(GeocachingComApiCaches));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock1.Text = "" + Measure(NewtonsoftJson);
+        }
+    }
+}
