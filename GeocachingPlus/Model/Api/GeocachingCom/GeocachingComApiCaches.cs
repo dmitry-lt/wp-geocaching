231d1f849a26e8aa162aab94d38f2a7ae0b53e6c
dmitry.kichinsky <dmitry.kichinsky@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2012-12-06 15:05:42 +0000

    geocaching.com

--- GeocachingPlus/Model/Api/GeocachingCom/GeocachingComApiCaches.cs
+++ GeocachingPlus/Model/Api/GeocachingCom/GeocachingComApiCaches.cs
@@ -26,4 +26,25 @@ namespace GeocachingPlus.Model.Api.GeocachingCom
         public string n { get; set; }
     }
 
+
+    [DataContract]
+    public class GeocachingComLogbook
+    {
+        [DataMember]
+        public string status { get; set; }
+
+        [DataMember]
+        public GeocachingComLogbookEntry[] data { get; set; }
+    }
+
+    [DataContract]
+    public class GeocachingComLogbookEntry
+    {
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string LogText { get; set; }
+    }
+
 }
