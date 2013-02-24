0a0fd05739c1e8490d169c02d6fe63d17bfc0b69
Yekaterina.Kanaeva@gmail.com <Yekaterina.Kanaeva@gmail.com@91f03f16-3611-a9a2-b6f4-7652f3aa398e>
2013-02-24 18:23:24 +0000

    UnauthorizedAccessException in webBrowser.NavigateToString

---
+++ branches/dev_opencaching/postRequest/postRequest/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO;
+
+namespace postRequest
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void showText(string str)
+        {
+           this.webBrowser1.NavigateToString(str);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Request request = new Request();
+            Action<string> onResponseGot;
+             onResponseGot = delegate(string s) { showText(s); };
+            request.Post("http://www.opencaching.de/map2.php", "", onResponseGot);
+        }
+        
+
+    }
+}
